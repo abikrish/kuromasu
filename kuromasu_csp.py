ad4d287f068470855b84b86b07945e63fd6c890c
abhi <abikrish@gmail.com>
2016-03-15 00:51:11 -0400

    added basic csp files

---
+++ kuromasu_csp.py
@@ -0,0 +1,12 @@
+#Look for #IMPLEMENT tags in this file. These tags indicate what has
+#to be implemented.
+
+'''
+Construct and return kuromasu CSP models.
+'''
+
+from cspbase import *
+import itertools
+
+def kuromasu_csp_model_1(initial_futoshiki_board):
+    1==1
